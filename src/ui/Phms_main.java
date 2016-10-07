defc9932b20f3d011281f3dc4271c235a0cf42b0
Dinesh Prasanth <mkdineshprasanth@gmail.com>
2016-10-07 17:47:07 -0400

    Initial Commit

---
+++ src/ui/Phms_main.java
@@ -0,0 +1,19 @@
+package ui;
+
+/* CSC540 - DBMS - P1 - Personal Health Management System
+ * Created by:
+ * Dinesh Prasanth M K
+ * Tianpei Xia
+ * Tyler Cannon
+ * Ruth Okoilu
+ * 
+ * 
+ */
+public class Phms_main {
+
+	public static void main(String[] args) {
+		System.out.println("Hellow World!");
+
+	}
+
+}
