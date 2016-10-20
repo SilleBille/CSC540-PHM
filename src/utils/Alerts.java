b3e0786a36950af95f0eadf7c7d9471cc229dce2
Tyler Cannon <tjcannon@ncsu.edu>
2016-10-19 23:34:10 -0400

    Methods formed and waiting for SQL. Need help forming alerts and health indicators. Need further table understanding.

---
+++ src/utils/Alerts.java
@@ -0,0 +1,25 @@
+package utils;
+
+
+import java.util.ArrayList;
+
+/**
+ * Created by Tyler on 10/19/2016.
+ *
+ * Needs UID from main menu login.
+ */
+
+public class Alerts {
+
+    ArrayList<String> alerts = new ArrayList<>();
+
+    //needs UID from main menu login
+
+    //execute sql query to receive alerts for uid into array - alerts[]
+
+    for(int i=0,i < alerts.size(),i++)
+    {
+        System.out.print(alerts);
+    }
+
+}
---
+++ src/utils/HealthSup.java
@@ -0,0 +1,66 @@
+package utils;
+
+import java.util.Scanner;
+
+/**
+ * Created by Tyler on 10/19/2016.
+ *
+ * Needs uid passed from main login
+ *
+ */
+public class HealthSup {
+
+    public static void profileMod() {
+        int selection = 1;
+        Scanner s = new Scanner(System.in);
+
+        while (selection != 3) {
+            System.out.print("Profile Menu\n\n" +
+                    "Would you like to add or edit supporters (1-2):\n" +
+                    "1. View Supporters\n" +
+                    "2. Edit Add Supports\n" +
+                    "3. Exit to Main Menu");
+
+            selection = s.nextInt();
+
+            switch (selection) {
+                case 1:
+                    viewSupporters();
+                    break;
+                case 2:
+                    editSupporters();
+                    break;
+                case 3:
+                    break;
+                default:
+                    break;
+            }//end switch
+        }
+    }
+
+    private static void viewSupporters()
+    {
+        //needs uid to find supporters for user
+        //execute sql statement to return supporters to uid
+        System.out.print("results here");
+    }
+
+    private static void editSupporters()
+    {
+        int selection = 1;
+        Scanner s = new Scanner(System.in);
+
+        //needs uid to find supporters for user
+        //execute sql statement to return supporters to uid
+        System.out.println("name and sid here");
+
+        System.out.println("Which supporter would you like to edit?");
+
+        selection = s.nextInt(); //hold sid to edit
+
+        //form sql around sid selection
+
+        //edit fields here
+    }
+
+}
