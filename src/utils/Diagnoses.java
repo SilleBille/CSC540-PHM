c7ae0c1212cee3afe04241321a0a4d6119c8ef07
Tyler Cannon <tjcannon@ncsu.edu>
2016-10-19 20:41:35 -0400

    Class designed to handle diagnosis viewing, addition, and removal. Needs SQL Queries to be complete.

---
+++ src/utils/Diagnoses.java
@@ -0,0 +1,77 @@
+package utils;
+
+import java.util.Scanner;
+
+/**
+ * Created by Tyler on 10/19/2016.
+ */
+public class Diagnoses {
+
+    public static void profileMod()
+    {
+        int selection = 1;
+
+        Scanner s = new Scanner(System.in);
+
+        while (selection != 3) {
+            System.out.print("Profile Menu\n\n" +
+                    "Please make a selection (1-6):\n" +
+                    "1. View Profile\n" +
+                    "2. Add Diagnoses\n" +
+                    "3. Remove Diagnoses\n" +
+                    "4. Exit to Main Menu");
+
+            selection = s.nextInt();
+
+            switch (selection) {
+                case 1:
+                    viewDiagnoses();
+                    break;
+                case 2:
+                    addDiagnoses();
+                    break;
+                case 3:
+                    removeDiagnoses();
+                    break;
+                case 4:
+                    break;
+                default:
+                    break;
+            }//end switch
+        }
+    }
+
+    private static void viewDiagnoses()
+    {
+        //print query results here
+        System.out.print("Query Results Here");
+    }
+
+    private static void addDiagnoses()
+    {
+        System.out.println("Please enter a Diagnosis to add.");
+        String diag; // holds diagnosis name
+
+        Scanner s = new Scanner(System.in);
+
+        diag = s.nextLine();
+
+        //repeat last 3 lines for all parameters and input to sql insert
+
+        System.out.println("Diagnosis "+ diag + " added.");
+    }
+
+    private static void removeDiagnoses()
+    {
+        System.out.println("Please enter a Diagnosis to remove.");
+        String diag; // holds diagnosis name
+
+        Scanner s = new Scanner(System.in);
+
+        diag = s.nextLine();
+
+        //sql here
+
+        System.out.println("Diagnosis removed.\n\n");
+    }
+}
