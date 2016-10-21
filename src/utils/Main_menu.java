4c58226bd219a26a3157ec80fc22e65d5108e79f
Tyler Cannon <tjcannon@ncsu.edu>
2016-10-20 21:14:12 -0400

    Update Main_menu to handle supporters

--- src/utils/Main_menu.java
+++ src/utils/Main_menu.java
@@ -1,10 +1,7 @@
 package utils;
 import java.util.Scanner;
 
-/**package utils;
- import java.util.Scanner;
-
- /**
+/**
  * Created by Tyler on 10/18/2016.
  *
  * Description:
@@ -15,50 +12,97 @@ import java.util.Scanner;
  *
  * Inputs:
  * int - via scanner
+ * int - uid from main method
  */
 public class Main_menu {
 
     public static void displayMenu() {
         int selection = 1;
+        int uid = 0;
+        boolean isSupporter = false;
 
         Scanner s = new Scanner(System.in);
 
-        while (selection != 6)
-        {
-            System.out.print("Main Menu\n\n"+
-                    "Please make a selection (1-6):\n"+
-                    "1. Profile\n"+
-                    "2. Diagnoses\n"+
-                    "3. Health Indicator\n"+
-                    "4. Alerts\n"+
-                    "5. Health Supporters\n"+
-                    "6. Logout\n");
+        //insert sql statement to return boolean value of whether uid belongs to a health supporter or not.
+        if (isSupporter = false) {
+            while (selection != 6) {
+                System.out.print("Main Menu\n\n" +
+                        "Please make a selection (1-6):\n" +
+                        "1. Profile\n" +
+                        "2. Diagnoses\n" +
+                        "3. Health Indicator\n" +
+                        "4. Alerts\n" +
+                        "5. Health Supporters\n" +
+                        "6. Logout\n");
 
+                selection = s.nextInt();
 
-            selection = s.nextInt();
+                switch (selection) {
+                    case 1:
+                        Profile.profileMod();
+                        break;
+                    case 2:
+                        Diagnoses.diagnosisMod();
+                        break;
+                    case 3:
+                        //health_ind();
+                        break;
+                    case 4:
+                        //alerts();
+                        break;
+                    case 5:
+                        //health_sup();
+                        break;
+                    case 6:
+                        //logout();
+                        break;
+                    default:
+                        break;
+                }//end switch
+            }//end while
+        }
+        else{
+            while (selection != 7) {
+                System.out.print("Main Menu\n\n" +
+                        "Please make a selection (1-6):\n" +
+                        "1. Profile\n" +
+                        "2. Diagnoses\n" +
+                        "3. Health Indicator\n" +
+                        "4. Alerts\n" +
+                        "5. Health Supporters\n" +
+                        "6. Patient Edit"+
+                        "7. Logout\n");
 
-            switch(selection) {
-                case 1:
-                    Profile.profileMod();
-                    break;
-                case 2:
-                    Diagnoses.diagnosisMod();
-                    break;
-                case 3:
-                    health_ind();
-                    break;
-                case 4:
-                    alerts();
-                    break;
-                case 5:
-                    health_sup();
-                    break;
-                case 6:
-                    logout();
-                    break;
-                default:
-                    break;
-            }//end switch
-        }//end while
+                selection = s.nextInt();
+
+                switch (selection) {
+                    case 1:
+                        Profile.profileMod();
+                        break;
+                    case 2:
+                        Diagnoses.diagnosisMod();
+                        break;
+                    case 3:
+                        //health_ind();
+                        break;
+                    case 4:
+                        //alerts();
+                        break;
+                    case 5:
+                        //health_sup();
+                        break;
+                    case 6:
+                        //patient_mod;
+                        break;
+                    case 7:
+                        //logout();
+                        break;
+                    default:
+                        break;
+                }//end switch
+            }//end while
+        }//end if
     }//end main
 }//end class
+
+
