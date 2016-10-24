2841ac53485fe2483d82cbd71fb77db6968d731c
Tyler Cannon <tjcannon@ncsu.edu>
2016-10-23 21:42:09 -0400

    Fixed menu numbering in supporter.

--- src/utils/Alerts.java
+++ src/utils/Alerts.java
@@ -1,6 +1,13 @@
 package utils;
 
+import sql.SqlQueries;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Scanner;
 import java.io.*;
 
@@ -13,29 +20,68 @@ import java.io.*;
 
 public class Alerts {
 
-    public static void view(int userID) {
+    static PreparedStatement ps;
+    static ResultSet rs;
+    static Connection con = ConnectionClass.conn;
+
+    static public Integer getInteger(ResultSet rs, String strColName) throws Exception {
+        int nValue = rs.getInt(strColName);
+        return rs.wasNull() ? null : nValue;
+    }
+
+    public static void view(int userID) throws Exception{
         String response = null;
         Scanner s = new Scanner(System.in);
         ArrayList<String> alerts = new ArrayList<>();
         ArrayList<String> keys = new ArrayList<>();
 
-        //TODO needs UID from main menu login
+        //return all alerts for give UID
+        try {
+            ps = con.prepareStatement(SqlQueries.SQL_VIEW_ALERT_FOR_UID);
+            ps.setInt(1, Userid.USER_ID_STATIC);
+            rs = ps.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
-        //TODO execute sql query to receive alerts for uid into array - alerts[]
-        //TODO extract key field for each alert to keys arraylist
+        int currAID = 0;
+        while(rs.next())
+        {
+            currAID = getInteger(rs, "AID");
+            System.out.println("AID = " + currAID);
 
-        for (int i = 0; i < alerts.size(); i++) {
-            System.out.println(alerts.get(i));
-            System.out.println("Would you like to clear this alert? (Y/N)");
+            Date tempDate = rs.getDate("ALERT_DATE");
+            System.out.println("Alert Date = " + tempDate.toString());
+
+            String tempType = rs.getString("ALERT_TYPE");
+            System.out.println("Alert Type = " + tempType);
+
+            int currUID = getInteger(rs, "U_ID");
+            System.out.println("UID = " + currUID);
+
+            String tempStatus = rs.getString("STATUS");
+            System.out.println("Alert Status = " + tempType);
+
+            int tempPid = getInteger(rs, "PID");
+            System.out.println("PID = " + tempPid);
+
+            String tempDesc = rs.getString("DESCRIPTION");
+            System.out.println("Description = " + tempDesc);
+
+            System.out.println("\n\nWould you like to clear this alert? (Y/N)");
             response = s.nextLine().toUpperCase();
 
             if (response.equals("Y"))
             {
-                //TODO insert sql to update alert cleared field
+                ps = con.prepareStatement(SqlQueries.SQL_CLEAR_ALERT);
+                ps.setInt(1, currAID);
+                rs = ps.executeQuery();
                 System.out.println("Update cleared.");
             }
             System.out.print("\n\n\n");
+
         }
+
     }
 
 }
